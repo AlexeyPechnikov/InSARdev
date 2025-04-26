Tune zarr compression, fix interferogram calculation and plotting

insardev/insardev/Stack_plot.py
@@ -148,7 +148,7 @@ class Stack_plot(Stack_export):
             factor_x = int(np.round(size_x / (_size[0] / cols)))
             #print ('factor_x, factor_y', factor_x, factor_y)
             # coarsen and materialize data for all the calculations and plotting
-            progressbar(da := da[:,::max(1, factor_y), ::max(1, factor_x)].persist(), desc=f'Computing {polarization}'.ljust(25))
+            progressbar(da := da[:,::max(1, factor_y), ::max(1, factor_x)].persist(), desc=f'Computing {polarization} Plot'.ljust(25))
 
             # calculate min, max when needed
             if quantile is not None:
@@ -173,6 +173,7 @@ class Stack_plot(Stack_export):
             fg.fig.suptitle(f'{polarization} {caption}', y=y)            
             #self.plots_AOI(fg, **kwargs)
             #self.plots_POI(fg, **kwargs)
+            return fg
 
         if quantile is not None:
             assert vmin is None and vmax is None, "ERROR: arguments 'quantile' and 'vmin', 'vmax' cannot be used together"
@@ -197,28 +198,31 @@ class Stack_plot(Stack_export):
         #print ('polarizations', polarizations)
 
         # process polarizations one by one
+        fgs = []
         for pol in polarizations:
-            plot_polarization(data, polarization=pol)
+            fg = plot_polarization(data, polarization=pol)
+            fgs.append(fg)
+        return fgs
 
     def plot_displacement_mm(self, data, polarizations=None,
                    cmap='turbo', vmin=None, vmax=None, quantile=None, symmetrical=False,
                    caption='Displacement, [mm]', cols=4, rows=4, size=4, nbins=5, aspect=1.2, y=1.05, _size=None, **kwargs):
         data_los_mm = self.los_displacement_mm(data)
-        self.plot_stack(data_los_mm, polarizations,
+        return self.plot_stack(data_los_mm, polarizations,
                         cmap=cmap, vmin=vmin, vmax=vmax, quantile=quantile, symmetrical=symmetrical,
                         caption=caption, cols=cols, rows=rows, size=size, nbins=nbins, aspect=aspect, y=y, wrap=True, _size=_size, **kwargs)
 
     def plot_displacement(self, data, polarizations=None,
                    cmap='turbo', vmin=None, vmax=None, quantile=None, symmetrical=False,
                    caption='Displacement, [rad]', cols=4, rows=4, size=4, nbins=5, aspect=1.2, y=1.05, _size=None, **kwargs):
-        self.plot_stack(data, polarizations,
+        return self.plot_stack(data, polarizations,
                         cmap=cmap, vmin=vmin, vmax=vmax, quantile=quantile, symmetrical=symmetrical,
                         caption=caption, cols=cols, rows=rows, size=size, nbins=nbins, aspect=aspect, y=y, wrap=True, _size=_size, **kwargs)
 
     def plot_phase(self, data, polarizations=None,
                    cmap='turbo', vmin=None, vmax=None, quantile=None, symmetrical=False,
                    caption='Phase, [rad]', cols=4, rows=4, size=4, nbins=5, aspect=1.2, y=1.05, _size=None, **kwargs):
-        self.plot_stack(data, polarizations,
+        return self.plot_stack(data, polarizations,
                         cmap=cmap, vmin=vmin, vmax=vmax, quantile=quantile, symmetrical=symmetrical,
                         caption=caption, cols=cols, rows=rows, size=size, nbins=nbins, aspect=aspect, y=y, wrap=True, _size=_size, **kwargs)
 
@@ -226,7 +230,7 @@ class Stack_plot(Stack_export):
                            cmap='gist_rainbow_r',
                            caption='Phase, [rad]', cols=4, rows=4, size=4, nbins=5, aspect=1.2, y=1.05, _size=None, **kwargs):
         import numpy as np
-        self.plot_stack(data, polarizations,
+        return self.plot_stack(data, polarizations,
                         cmap=cmap, vmin=-np.pi, vmax=np.pi, quantile=None, symmetrical=False,
                         caption=caption, cols=cols, rows=rows, size=size, nbins=nbins, aspect=aspect, y=y, wrap=True, _size=_size, **kwargs)
 
@@ -239,7 +243,7 @@ class Stack_plot(Stack_export):
                 name='custom_gray', 
                 colors=['black', 'whitesmoke']
             )
-        self.plot_stack(data, polarizations,
+        return self.plot_stack(data, polarizations,
                         cmap=cmap, vmin=vmin, vmax=vmax, quantile=quantile, symmetrical=False,
                         caption=caption, cols=cols, rows=rows, size=size, nbins=nbins, aspect=aspect, y=y, wrap=False, _size=_size, **kwargs)
 

insardev_pygmtsar/insardev_pygmtsar/S1_transform.py
@@ -15,7 +15,7 @@ class S1_transform(S1_topo):
     import numpy as np
 
     def transform(self, ref: str, records: pd.DataFrame|None=None, degrees: float=12.0/3600,
-            resolution: tuple[int, int]=(20, 5), epsg: str|int|None='auto', timeout: str|int|None='30s', debug: bool=False):
+            resolution: tuple[int, int]=(20, 5), scale_factor: float=2.0, epsg: str|int|None='auto', timeout: str|int|None='30s', debug: bool=False):
         from tqdm.auto import tqdm
         import dask
         from tqdm.auto import tqdm
@@ -52,7 +52,7 @@ class S1_transform(S1_topo):
                 for burst_ref in burst_refs:
                     self.align_ref(burst_ref[-1], basedir, debug=debug)
                 #print ('compute_transform')
-                self.compute_transform(burst_refs[0][-1], basedir=basedir, resolution=resolution, epsg=epsg)
+                self.compute_transform(burst_refs[0][-1], basedir=basedir, resolution=resolution, scale_factor=scale_factor, epsg=epsg)
                 # for topo phase calculation
                 #print ('compute_transform_inverse')
                 self.compute_topo(burst_refs[0][-1], basedir=basedir, resolution=resolution)
@@ -177,16 +177,17 @@ class S1_transform(S1_topo):
             data_proj[varname].attrs['scale_factor'] = scale
             data_proj[varname].attrs['add_offset'] = 0
             data_proj[varname].attrs['_FillValue'] = np.iinfo(np.int16).min
-
-        encoding = {var: self.get_encoding_zarr(data_proj[var].shape, dtype=data_proj[var].dtype) for var in data_proj.data_vars}
-        # coord arrays: one chunk, no compressor, NaN fill value instead of default 0
-        #encoding.update({
-        #    'x': dict(compressor=None, chunks=(data_proj.sizes['x'],), dtype="float64", fill_value=np.nan),
-        #    'y': dict(compressor=None, chunks=(data_proj.sizes['y'],), dtype="float64", fill_value=np.nan),
-        #})
+        
+        encoding_vars = {var: self.get_encoding_zarr(chunks=(data_proj.x.size,),
+                                                     dtype=data_proj[var].dtype,
+                                                     shuffle='noshuffle'
+                                                     ) for var in data_proj.data_vars}
+        #print ('encoding_vars', encoding_vars)
+        encoding_coords = {coord: self.get_encoding_zarr(chunks=(data_proj[coord].size,), dtype=data_proj[coord].dtype) for coord in data_proj.coords}
+        #print ('encoding_coords', encoding_coords)
         data_proj.to_zarr(
             store=os.path.join(self.workdir, f'{resolution[0]}x{resolution[1]}', self.fullBurstId(burst_rep), burst_rep),
-            encoding=encoding,
+            encoding=encoding_vars | encoding_coords,
             mode='w',
             consolidated=True
         )

insardev_toolkit/insardev_toolkit/datagrid.py
@@ -52,21 +52,18 @@ class datagrid:
     # ['lz4', 'lz4hc', 'blosclz', 'zstd', 'zlib']
     zarr_compression_algorithm: str = 'zstd'
     zarr_clevel: int = 6
-    zarr_shuffle_floating: str = 'bitshuffle'
-    zarr_shuffle_integer: str = 'noshuffle'
+    zarr_shuffle: str = 'bitshuffle'
     zarr_blocksize: int = 0
 
     # define lost class variables due to joblib via arguments
-    def get_encoding_zarr(self, shape=None, dtype=None, shuffle=None, fill_value=None):
+    def get_encoding_zarr(self, chunks=None, dtype=None, shuffle=None, fill_value=None, compression=True):
         import numpy as np
         from zarr.codecs import BloscCodec
 
         if dtype is not None:
             if np.issubdtype(dtype, np.floating):
-                shuffle = self.zarr_shuffle_floating if shuffle is None else shuffle
                 fill_value = np.nan if fill_value is None else fill_value
             else:
-                shuffle = self.zarr_shuffle_integer if shuffle is None else shuffle
                 #fill_value = 0 if fill_value is None else fill_value
                 if fill_value is None:
                     # unsigned ints get 0, signed ints get the min representable
@@ -75,7 +72,7 @@ class datagrid:
                     else:
                         fill_value = np.iinfo(dtype).min
         if shuffle is None:
-            shuffle = self.zarr_shuffle_floating
+            shuffle = self.zarr_shuffle
         if fill_value is None:
             fill_value = np.nan
 
@@ -87,8 +84,12 @@ class datagrid:
         )
 
         opts = dict(fill_value=fill_value)
-        if self.zarr_compression_algorithm is not None and self.zarr_clevel >= 0:
+        if chunks is not None:
+            opts['chunks'] = chunks
+        if compression and self.zarr_compression_algorithm is not None and self.zarr_clevel >= 0:
             opts['compressors'] = (compressor,)
+        else:
+            opts['compressor'] = None
         return opts
 
     # define lost class variables due to joblib via arguments